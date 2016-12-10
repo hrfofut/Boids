a7132e7e79fb1d238a718f8747498f339d2d5951
jfish7 <jonathanfisher@utexas.edu>
2016-12-10 14:36:32 -0600

    testing commits

--- src/procedure_geometry.cc
+++ src/procedure_geometry.cc
@@ -3,12 +3,14 @@
 
 void create_boid_shape(std::vector<glm::vec4>& boid_shape_vertices, std::vector<glm::uvec3>& boid_shape_faces)
 {
-	boid_shape_vertices.push_back(glm::vec4(0, 0, 0, 1));
-    boid_shape_vertices.push_back(glm::vec4(1, -1, 0, 1));
-    boid_shape_vertices.push_back(glm::vec4(0, 2, 0, 1));
-    boid_shape_vertices.push_back(glm::vec4(-1, -1, 0, 1));
-    boid_shape_faces.push_back(glm::uvec3(0,1,2));
-    boid_shape_faces.push_back(glm::uvec3(0,2,3));
+//    boid_shape_vertices.push_back(glm::vec4(0, 0, 0, 1));
+//    boid_shape_vertices.push_back(glm::vec4(1, -1, 0, 1));
+//    boid_shape_vertices.push_back(glm::vec4(0, 2, 0, 1));
+//    boid_shape_vertices.push_back(glm::vec4(-1, -1, 0, 1));
+//    boid_shape_faces.push_back(glm::uvec3(0,1,2));
+//    boid_shape_faces.push_back(glm::uvec3(0,2,3));
+
+    boid_shape_vertices.push_back(glm::vec4(0, 0, 0, 1));
 }
 
 void create_food_shape(std::vector<glm::vec4>& obj_vertices, std::vector<glm::uvec3>& obj_faces) {
